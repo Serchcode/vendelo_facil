f8000dcdcdaf59c55c98b6338cd05863784c7269
MartinMelo97 <nitram-210397@hotmail.com>
2017-02-27 13:13:40 -0600

    AJAX

--- products/ajax.py
+++ products/ajax.py
@@ -4,16 +4,19 @@ from .models import Categoria_Anuncio, SubCategoria_Anuncio
 
 def get_subcategoria_anuncio(request):
     categoria_id = request.GET.get('categoria_id')
-    print(categoria_id)
+    print("qye"+categoria_id)
     subcategorias = SubCategoria_Anuncio.objects.none()
-    options = '<option value="" selected="selected"></option>'
+    options = ''
+    li = '<li></li>'
     if categoria_id:
         subcategorias = SubCategoria_Anuncio.objects.filter(categoria_id=categoria_id)
     for subcategoria in subcategorias:
+        li += '<li><span>%s</span></li>' % (subcategoria.nombre_subcategoria)
         options += '<option value="%s">%s</option>' % (
             subcategoria.pk,
             subcategoria.nombre_subcategoria
         )
     response = {}
     response['subcategorias'] = options
+    response['lista'] = li
     return JsonResponse(response)
