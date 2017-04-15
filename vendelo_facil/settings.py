ce6478fd4afbfd304c1f468db6b195cd33c7d740
MartinMelo97 <nitram-210397@hotmail.com>
2017-04-15 15:30:38 -0500

    News

--- vendelo_facil/settings.py
+++ vendelo_facil/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'vendelo_facil.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 if DEBUG:
-
+    
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
@@ -89,7 +89,6 @@ if DEBUG:
         }
     }
 else:
-    
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
