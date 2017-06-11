f401e9a9e98b28dd44ce6d8bb94a86bf0ee91248
MartinMelo97 <nitram-210397@hotmail.com>
2017-06-10 21:08:34 -0500

    password1

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from django.contrib.auth.views import logout, login , logout_then_login
+from django.contrib.auth.views import logout, login , logout_then_login, password_change, password_change_done
 from . import views
 from products import urls as productsurls
 urlpatterns = [
@@ -8,5 +8,7 @@ urlpatterns = [
 	url(r'^login/$', login, name="login"),
 	url(r'^logout/$', logout, name="logout"),
 	url(r'^logout-then-login/$', logout_then_login, name="logout_then_login"),
+	url(r'^password-change/$', password_change, name="password_change"),
+	url(r'^password_change/done/$', password_change_done, name="password_change_done"),
 	url(r'^product/',include(productsurls, namespace="product")),
 ]
