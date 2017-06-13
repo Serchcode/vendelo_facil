08d0358a9e0f265d2b1458bfa3d5bb10ca0c4a7c
MartinMelo97 <nitram-210397@hotmail.com>
2017-06-13 02:24:40 -0500

    Quedo cambio, y vamos mitad de reseteo

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, include
 from django.contrib.auth.views import logout, login , logout_then_login, password_change, password_change_done
+from django.contrib.auth.views import password_reset, password_reset_done, password_reset_confirm, password_reset_complete
 from . import views
 from products import urls as productsurls
 urlpatterns = [
@@ -9,6 +10,10 @@ urlpatterns = [
 	url(r'^logout/$', logout, name="logout"),
 	url(r'^logout-then-login/$', logout_then_login, name="logout_then_login"),
 	url(r'^password-change/$', password_change, name="password_change"),
-	url(r'^password_change/done/$', password_change_done, name="password_change_done"),
+	url(r'^password-change/done/$', password_change_done, name="password_change_done"),
 	url(r'^product/',include(productsurls, namespace="product")),
+	url(r'^password-reset/$', password_reset, name='password_reset'),
+	url(r'^password-reset/done/$', password_reset_done, name="password_reset_done"),
+	url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', password_reset_confirm, name="password_reset_confirm"),
+	url(r'^password-reset/complete/$', password_reset_complete, name="password_reset_complete")	
 ]
